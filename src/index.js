aea10517a56b0d87c3141a5d5eea531f17ba3474
Miroslav <77319935+NitroDaxs@users.noreply.github.com>
2025-04-06 12:41:25 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+let React = require("react");
+let ReactDom = require("react-dom");
+
+ReactDom.render(
+  <div>
+    <h1>Hello world</h1>
+    <p>This is my paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
